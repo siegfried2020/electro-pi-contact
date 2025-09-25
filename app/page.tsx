66762e5420900fdddb9404459f0781f2dc2df221
siegfried2020 <domydodo2015@gmail.com>
2025-09-25 21:22:14 +0300

    contact-1

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,151 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <div className="m-10 grid grid-cols-1 gap-y-6 items-center justify-items-center">
+        <button className="text-gray-300 p-1 px-6 rounded-2xl border-1 text-xl border-amber-50">تواصل معنا</button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h1 className="text-5xl"><span className="text-blue-700">نقدّم لك</span> التقنية… بشكل يناسبك</h1>
+        <p className="text-gray-400 text-xl">كل اللي تحتاجه في مكان واحد – خدمات رقمية متكاملة مصممة لك وتغطي كل جوانب مشروعك</p>
+      </div>
+      
+      <div className="bg-[url(/continents.png)] bg-cover inset-shadow-[0_120px_35px_rgba(0,0,0,0.7)] 
+      grid grid-cols-2 justify-items-center ">
+        <div className="grid grid-rows-2 gap-y-4 contact-form ml-50 mt-15">
+          
+          <div>
+            <label>Name</label>
+            <br />
+            <input type="text"  placeholder="ex: John Adam"/>
+          </div>
+        
+          <div>
+            <label>Email</label>
+            <br />
+            <input type="email"  placeholder="ex: john@gmail.com"/>
+          </div>
+          
+  
+          <div>
+            <label>Message</label>
+            <br />
+            {/* <input type="text" className="h-44 rounded-2xl"/> */}
+            <textarea className="h-44 resize-none p-2 w-80 rounded-2xl bg-[#2B2A4A]/80"></textarea>
+          </div>
+
+          <div>
+            <button className="bg-[#2154FE] hover:bg-[#002bb6] cursor-pointer p-2 px-28 rounded-3xl text-2xl">إرسال رسالة</button>
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+        <div className="bg-[#131422]/80 rounded-2xl py-5 pl-10 pr-30 grid grid-rows-4 gap-10 mr-50 mt-15">
+          <div>
+            <h1 className="text-3xl">Search Us</h1>
+            
+          </div>
+          
+          <div className="Email info">
+            <div>
+              <Image
+                className=""
+                src="/email.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+            <div>
+              <h5>E-mail</h5>
+              <h5 className="text-[#9F9F9F]">info@inforamtion.sa</h5>
+            </div>
+          </div>
+
+          <div className="Phone info">
+            <div>
+              <Image
+                className=""
+                src="/phone.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+            
+            <div>
+              <h5>Phone</h5>
+              <h5 className="text-[#9F9F9F]">+966 5388 75889</h5>
+            </div>
+          </div>
+
+          <div className="location info">
+            <div>
+              <Image
+                className=""
+                src="/location.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+            
+            <div>
+              <h5>location</h5>
+              <h5 className="text-[#9F9F9F]">info@inforamtion.sa</h5>
+            </div>
+          </div>
+
+          <div className="Media flex gap-x-10">
+            <div>
+              <Image
+                className=""
+                src="/linkedin.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+            <div>
+              <Image
+                className=""
+                src="/instagram logo.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+            <div>
+              <Image
+                className=""
+                src="/facebook logo.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+
+            <div>
+              <Image
+                className=""
+                src="/X logo.svg"
+                alt="Next.js logo"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
